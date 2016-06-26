f8a858cab8ee18e49b6681774b5f63d2df99076a
Thomas <Thomas@DevBook>
2016-06-26 22:55:51 +0200

    added some more props

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -3,11 +3,14 @@ import * as React from "react";
 export interface PanelProps {
     orientation: string;
     color?: string;
+    rowHeight: string;
+    colWidth: string;
+    flex: string;
 }
 
 export class Panel extends React.Component<PanelProps, {}> {
     render() {
-        return <div className={"flex panel row-sm col-xl " + this.props.color + " " + this.props.orientation}>
+        return <div className={"panel row-" + this.props.rowHeight + " col-" + this.props.colWidth + " " + this.props.color + " " + this.props.orientation + " " + this.props.flex}>
             <div className="content">
             </div>
         </div>;
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import { Panel } from "./components/Panel";
 import "./styles/main.scss";
 
 ReactDOM.render(
-    <Panel color="red" orientation="top right" />,
+    <div className="flex layout-h">
+    <Panel color="red" orientation="top right" rowHeight="xxs" colWidth="lg" flex="flex-30" />
+    <Panel color="red" orientation="top left" rowHeight="xxs" colWidth="xl" flex="flex" />
+    </div>,
     document.getElementById("root")
 );
